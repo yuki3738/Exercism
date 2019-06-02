539c388314c65739cdf796f68ea55c0e78f5069b
yuki3738 <y.minamiya@gmail.com>
2019-06-02 17:49:18 +0900

    Change /ruby/pangram/

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -1,5 +1,5 @@
 class Pangram
   def self.pangram?(sentence)
-    sentence.split(/[^[:alpha:]]/).sort.uniq.join.lstrip == ('a'..'z').to_a.join
+    sentence.split(/[^[:alpha:]]/).join.downcase.split('').sort.uniq.join.lstrip == ('a'..'z').to_a.join
   end
 end
