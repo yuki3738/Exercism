45c36903ceec7ed2cc3e146a3ca7c032da0fdf86
yuki3738 <y.minamiya@gmail.com>
2019-06-06 01:39:32 +0900

    Change /ruby/series/series.rb

--- ruby/series/series.rb
+++ ruby/series/series.rb
@@ -1,6 +1,4 @@
 class Series
-  attr_reader :digits_ary
-
   def initialize(digits)
     @digits_ary = digits.chars
   end
@@ -8,12 +6,10 @@ class Series
   def slices(n)
     raise ArgumentError if n > digits_ary.length
 
-    slices = []
-    digits_ary.each_cons(n) do |digit|
-      slices << digit
-    end
-    slices.map do |slice|
-      slice.join
-    end
+    digits_ary.each_cons(n).map(&:join)
   end
+
+  private
+
+  attr_reader :digits_ary
 end
