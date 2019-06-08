7ce30ad6ef5c47817cb09de21bc588307f190dee
yuki3738 <y.minamiya@gmail.com>
2019-06-08 20:12:04 +0900

    Change ruby/space-age/space_age.rb

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -1,45 +1,30 @@
 class SpaceAge
-  attr_reader :seconds
+  ORBITAL_PERIOD = {
+    earth: 1.0,
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
+  }
 
   def initialize(seconds)
     @seconds = seconds.to_f
   end
 
-  def on_earth
-    calc_age
-  end
-
-  def on_mercury
-    calc_age(0.2408467)
-  end
-
-  def on_venus
-    calc_age(0.61519726)
-  end
-
-  def on_mars
-    calc_age(1.8808158)
-  end
-
-  def on_jupiter
-    calc_age(11.862615)
-  end
-
-  def on_saturn
-    calc_age(29.447498)
-  end
-
-  def on_uranus
-    calc_age(84.016846)
-  end
-
-  def on_neptune
-    calc_age(164.79132)
+  ORBITAL_PERIOD.each do |planet, earth_years|
+    define_method :"on_#{planet}" do
+      calc_age(earth_years)
+    end
   end
 
   private
 
-  def calc_age(orbital_period = 1)
-    seconds / 60 / 60 / 24 / (365.25 * orbital_period)
+  attr_reader :seconds
+
+  def calc_age(earth_years)
+    seconds / 60 / 60 / 24 / (365.25 * earth_years)
   end
 end
