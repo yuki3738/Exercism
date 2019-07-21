afddee01c2eb5e12aa5ad7f7a92289de395ef6ca
yuki3738 <y.minamiya@gmail.com>
2019-07-21 13:03:38 +0900

    Change /ruby/hamming/hamming.rb

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -2,6 +2,6 @@ class Hamming
   def self.compute(strand_one, strand_two)
     raise ArgumentError unless strand_one.size == strand_two.size
 
-    strand_one.chars.each_with_index.count { |s, i| s != strand_two.chars[i] }
+    strand_one.each_char.each_with_index.count { |s, i| s != strand_two.chars[i] }
   end
 end
